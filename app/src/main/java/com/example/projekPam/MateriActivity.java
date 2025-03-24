70d95b5f37c500b1d1f0c41b815e23b678ffdfb6
Althaf Rizal <ilikemud7@gmail.com>
2025-03-24 15:10:14 +0700

    update scroll

--- app/src/main/java/com/example/projekPam/MateriActivity.java
+++ app/src/main/java/com/example/projekPam/MateriActivity.java
@@ -1,6 +1,8 @@
 package com.example.projekPam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.projekPam.databinding.ActivityMateriBinding;
 
-public class MateriActivity extends AppCompatActivity {
+public class MateriActivity extends AppCompatActivity implements View.OnClickListener{
     private ActivityMateriBinding binding;
 
     @Override
@@ -19,7 +21,16 @@ public class MateriActivity extends AppCompatActivity {
         binding = ActivityMateriBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.btnBackMateri.setOnClickListener(this);
+
         String title = getIntent().getStringExtra("TITLE");
         binding.tvCardJudulMateri.setText(title);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.btnBackMateri)
+            finish();
     }
 }
